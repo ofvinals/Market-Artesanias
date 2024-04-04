ab66047c5cbd596baabefc4c143bbaffb62119dc
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-03 21:33:25 -0500

    compenetes agregados al app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Categorias from './components/Categorias';
+import Descuentos from './components/Descuentos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-3xl font-bold underline'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='px-4 md:px-24'>
+        <Categorias />
+        <Descuentos />
+      </div>
+    </>
   );
 }
 
