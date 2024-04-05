be04fe1b7be23320af681e0421afc67b4e0ecb5b
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-05 17:37:48 -0500

    configuracion colores personalizados

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:  ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        'primary': '#E98C00',
+        'secondary': '#FEEACC',
+        'general': '#563300',
+        'specific': '#E98C00'
+
+      },
+    },
   },
   plugins: [],
 }
 
+ 
