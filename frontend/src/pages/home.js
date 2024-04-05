ca8aa5da2319ed265114e94a62b88dcbb3295fc2
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-05 13:46:43 -0500

    rutas y ventanas agregadas (aun falta meterle el contenido a cada una). Quedan pendientes unas pocas

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Categorias from '../components/Categorias';
 import Descuentos from '../components/Descuentos';
+import NavBar from '../components/Navbar';
+import Header from '../components/Header';
 
 
 
 function HomePage() {
     return (
-        <div className='px-4 md:px-24'>
-            <Categorias />
-            <Descuentos />
-        </div>
+        <>
+            <NavBar />
+            <Header />
+            <div className='px-4 md:px-24'>
+                <Categorias />
+                <Descuentos />
+            </div>
+        </>
     );
 }
 
