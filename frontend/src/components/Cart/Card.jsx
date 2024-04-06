35432530a374976a669ceba895dc2eb7962bb6c0
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-05 21:24:23 -0500

    Avance con la seccion de confirmacion de compra, falta darle unos detalles (y hacerlo funcional)

---
+++ frontend/src/components/Cart/Card.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import { MdDelete } from "react-icons/md";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const products = [
+    {
+        nombre: 'Vestido Vintage',
+        description: 'Talla L',
+        price: 16.42,
+        image: 'https://images.unsplash.com/photo-1565462905097-5e701c31dcfb?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        stok: 0
+    },
+    {
+        nombre: 'Botas Vintage',
+        description: 'Talla 6',
+        price: 66.99,
+        image: 'https://plus.unsplash.com/premium_photo-1674719144437-d1c253a8b775?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        stok: 2
+    },
+    {
+        nombre: 'Sombrero Vintage',
+        description: 'Talla 14',
+        price: 20.50,
+        image: 'https://images.unsplash.com/photo-1678705385245-ee7b75e35c80?q=80&w=2107&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        stok: 3
+    }
+
+]
+
+
+function Card() {
+    const [count, setCount] = useState(products.stok)
+
+    const plus = () => {
+        setCount(count + 1)
+    }
+    const minus = () => {
+        setCount(count - 1)
+    }
+    console.log(count);
+
+    return (
+        <section className='flex flex-col mt-[49px]'>
+            <h1 className='text-4xl mb-4 text-[#9AA0A6]'>Confirmacion de compra</h1>
+            <div className='flex flex-wrap gap-8'>
+
+                {
+                    products.map((product, idx) => (
+                        <div key={idx} className='bg-[#ECECEC] border border-[#A9A9A9] rounded-lg flex flex-row md:p-4 justify-between md:w-[504px] md:h-[198px]'>
+                            <div className='flex flex-row '>
+                                <div className='flex flex-col '>
+                                    <h3 className='font-bold text-[20px] text-[#A9A9A9]'>{product.nombre}</h3>
+                                    <p className='mb-4 text-[16px] text-[#A9A9A9]'>{product.description}</p>
+                                    <p className='font-bold text-[20px] text-[#A9A9A9]'>${product.price}</p>
+                                    <MdDelete className='text-[#A9A9A9] size-8  cursor-pointer hover:text-red-500 mt-8' />
+                                </div>
+                                <div className='flex flex-row gap-6 items-end '>
+                                    <AiOutlineMinus onClick={minus} className='text-[#A9A9A9] cursor-pointer' />
+                                    <input type="text" placeholder={product.stok} className='text-[#A9A9A9] font-bold border border-[#A9A9A9] rounded-lg w-7 pl-2' />
+                                    
+                                    <AiOutlinePlus onClick={plus} className='text-[#A9A9A9] cursor-pointer' />
+                                </div>
+                            </div>
+                            <div>
+                                <img src={product.image} alt={product.nombre} className='md:w-[145px] md:h-[160px] md:object-cover rounded-md' />
+                            </div>
+
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+    )
+}
+
+export default Card
