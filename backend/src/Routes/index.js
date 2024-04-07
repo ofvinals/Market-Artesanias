7d7eb0c442a4fb651e4ba34666b0be05e1782533
javier-pignataro <jgp.workmail@gmail.com>
2024-04-07 00:09:48 -0300

    Disenio y creacion de arbol de directorios para el patron MVC. Creacion de tabla de Users default (en progreso) con Sequelize. Implementacion de inyeccion de dependencias

--- backend/src/Routes/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-
-const routes = Router();
-
-routes.use("/Product", ( req, res)=>{});
-
-module.exports = routes;
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const routes = require("./Routes/index.js");
+
+// Rutas
+const createUserRouter = require("./routes/users.js"); // el Router del recurso "Users" 
+const UserModel = require("./models/postgresql/users.js"); // la implementacion (o "Modelo", del patron M.V.C.) que almacena Usuarios en la base de datos
+                                                           //
+                                                           // pense en seguir importando debajo de esta linea las demas rutas y modelos para las entidades
+                                                           // que fuiste creando en el DER que faltan (como UsuarioCompras, Producto, Tienda, Categoria)
+                                                           // Habria que tambien seguir creando un archivo para cada uno dentro de su 
+                                                           // directorio correspondiente (como el ejemplo de "users.js")
 
 const server = express();
 
@@ -10,23 +18,24 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+      next();
 });
 
-server.use("/", routes);
+// Inyeccion de dependencias ( implementacion del modelo )
+server.use( "/users", createUserRouter({ userModel: UserModel }) );
 
 server.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+      const status = err.status || 500;
+      const message = err.message || err;
+      console.error(err);
+      res.status(status).send(message);
 });
 
 module.exports = server;
