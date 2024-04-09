90630d7f14e79db636023d8f1225073c304abc92
Oscar Frias Viñals <140022816+ofvinals@users.noreply.github.com>
2024-04-09 18:01:03 -0300

    login normal funcionando con back

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,34 +1,73 @@
-import { useState } from 'react';
+import {  useState } from 'react';
 import { apiURL } from '../api/apiURL';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { types } from '../redux/Actions/authTypes';
+import authFire from '../firebase/config';
+import {
+	GoogleAuthProvider,
+	signInWithPopup,
+	FacebookAuthProvider,
+} from 'firebase/auth';
 
 export const useAuth = () => {
 	const [currentUser, setCurrentUser] = useState();
 	const [errors, setErrors] = useState([]);
 	const navigate = useNavigate();
-	const dispatch = useDispatch();
 
 	const auth = async (values) => {
-		console.log(values);
 		try {
-			const res = await apiURL.post('/Login', values);
-			if (res.status === 200) {
-				const { accesoWJT, admin, vendedor } = res.data; 
-				localStorage.setItem('token', accesoWJT); 
-				setCurrentUser(res.data); 
-				dispatch({
-					type: types.basicAuth,
-					payload: { JWT: accesoWJT, admin, vendedor },
-				}); // Enviar acción de autenticación al reductor
-				return res.data;
+			const user = await apiURL.post('/Login', values);
+			console.log(user);
+			if (user.status === 200) {
+				const userData = user.data; 
+				await setCurrentUser(userData);
+				const { accesoWJT } = userData;
+				localStorage.setItem('token', accesoWJT);
+				return user;
 			}
 		} catch (error) {
 			console.log(error.response.data);
 			setErrors(error.response.data);
 			throw error;
 		}
+	};
+
+	const startGoogleAuth = () => {
+		return async () => {
+			try {
+				const provider = new GoogleAuthProvider();
+				const { user } = await signInWithPopup(authFire, provider);
+				const userEmail = user.email
+				console.log(userEmail)
+				// const userExists = await checkIfUserExists(userEmail);
+				//   if (!userExists) {
+				// 	 createUserInDatabase(userEmail);
+				//   } else {
+				const idToken = await user.getIdToken();
+				localStorage.setItem('token', idToken);
+				await setCurrentUser(userEmail );
+				console.log(currentUser);
+			// }
+			} catch (error) {
+				console.error(error);
+			}
+		};
+	};
+
+	const startFacebookAuth = () => {
+		return async () => {
+			try {
+				const provider = new FacebookAuthProvider();
+				console.log(provider);
+				const userCredential = await signInWithPopup(authFire, provider);
+				console.log(userCredential);
+				const user = userCredential.user;
+				const idToken = await user.getIdToken();
+				localStorage.setItem('token', idToken);
+				console.log(user);
+			} catch (error) {
+				console.error(error);
+			}
+		};
 	};
 
 	const logout = () => {
@@ -42,5 +81,7 @@ export const useAuth = () => {
 		logout,
 		currentUser,
 		errors,
+		startGoogleAuth,
+		startFacebookAuth,
 	};
 };
