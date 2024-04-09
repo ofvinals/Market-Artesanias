49663667fc8ebf3558b83a9215d4727e673dc424
Dionel22 <diondiaz293@gmail.com>
2024-04-09 10:42:59 -0300

    handler de tienda

--- backend/src/Routes/RouterTienda/RouterTienda.js
+++ backend/src/Routes/RouterTienda/RouterTienda.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { postAddTienda } = require("../../Handlers/HandlerTienda/HandlerTienda");
+const { postAddTienda, getByStore } = require("../../Handlers/HandlerTienda/HandlerTienda");
 
 const routerTienda = Router();
 
 //routerTienda.get("/", );
+routerTienda.get("/", getByStore);
 routerTienda.post("/", postAddTienda);
 //routerTienda.put("/", );
 
