0ff9e3caa705c0f5b58f36274518ead3bd70c9e2
javier-pignataro <jgp.workmail@gmail.com>
2024-04-09 16:58:52 -0300

    Controllers de Categorias (getAll y post)

--- backend/src/Handlers/HandlerCategoria/HandlerCategorias.js
+++ backend/src/Handlers/HandlerCategoria/HandlerCategorias.js
@@ -2,26 +2,27 @@ const { getAll, postAdd } = require("../../Controllers/ControllerCategorias/Cont
 
 //GET todas las categoria.
 const getAllCategoria = async (req, res) => {
-    try {
-        const response = await getAll();
-        return  res.status(200).json(response);
-    } catch (error) {
-        return  res.status(500).json({error: error.mensage});
-    }
+      try {
+            const response = await getAll();
+            console.log( response );
+            return  res.status(200).json(response);
+      } catch (error) {
+            return  res.status(500).json({error: error.mensage});
+      }
 };
 
 //POST Carga la categoria en la DB.
 const postAddCategoria = async (req, res) => {
-    const { Nombre } = req.body;
-    try {
-        const response = await postAdd(Nombre);
-        return res.status(200).json(response);
-    } catch (error) {
-        return res.status(500).json({error: error.mensage});
-    }
+      const { Nombre } = req.body;
+      try {
+            const response = await postAdd(Nombre);
+            return res.status(200).json(response);
+      } catch (error) {
+            return res.status(500).json({error: error.mensage});
+      }
 };
 
 module.exports = {
-    getAllCategoria,
-    postAddCategoria
+      getAllCategoria,
+      postAddCategoria
 }
