783dc9275bb196cc8b5212722978d64955a73120
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-10 20:11:56 -0500

    carrito y opciones de metodo de pago modificadas

--- frontend/src/components/Cart/Payment.jsx
+++ frontend/src/components/Cart/Payment.jsx
@@ -5,14 +5,14 @@ function Payment() {
     <section>
       <h2 className='text-tlv text-general mt-[82px] mb-[31px]'>Seleccionar método de pago</h2>
       <div className='flex flex-col gap-6'>
-        <button className='h-16 w-[504px]  border-primary border-2 rounded-[10px]'>
-          <p className='font-bold text-xl text-specific '>Pagar con tarjeta</p>
+        <button className='h-16 w-[504px]  border-primary border-2 rounded-[10px] font-bold text-xl text-specific'>
+          Pagar con tarjeta
         </button>
-        <button className='h-16 w-[504px]  border-primary border-2 rounded-[10px]'>
-          <p className='font-bold text-xl text-specific '>Pagar con Mercado Pago</p>
+        <button className='h-16 w-[504px]  border-primary border-2 rounded-[10px] font-bold text-xl text-specific'>
+          Pagar con Mercado Pago
         </button>
-        <button className='h-16 w-[504px] bg-primary rounded-[10px]'>
-          <p className='font-bold text-xl text-white'>Continuar</p>
+        <button className='h-16 w-[504px] bg-primary rounded-[10px] font-bold text-xl text-white'>
+          Continuar
         </button>
       </div>
     </section>
