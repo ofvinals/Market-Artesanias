079794b9c5cf742fe8e479612030579d81540f2e
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-19 19:17:44 -0500

    timestamps false

--- backend/src/Models/Category.js
+++ backend/src/Models/Category.js
@@ -1,16 +1,18 @@
 const { DataTypes } = require("sequelize")
 
 module.exports = categoryModel = (sequelize) => {
-  const Category =  sequelize.define("Category", {
+  const Category = sequelize.define("Category", {
     Id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     Nombre: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  });
+  },
+    { timestamps: false }
+  );
   return Category;
 }
