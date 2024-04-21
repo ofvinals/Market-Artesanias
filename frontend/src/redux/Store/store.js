74b9043d8bbc7f925bd1d5fe08c21edf443b5542
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-20 19:15:09 -0500

    agrego al store el slice del carrito

--- frontend/src/redux/Store/store.js
+++ frontend/src/redux/Store/store.js
@@ -4,6 +4,7 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import categoriesReducer from '../Slices/categoriesSlice'
 import productsReducer from '../Slices/productSlice'
+import cartReducer from '../Slices/cartSlice'
 
 const authPersistConfig = {
 	key: 'auth',
@@ -16,7 +17,8 @@ const store = configureStore({
 	reducer: {
 		auth: persistedAuthReducer,
 		categoria: categoriesReducer,
-		products: productsReducer
+		products: productsReducer,
+		cart: cartReducer
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
