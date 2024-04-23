b7b1c010b6aaf5cd85ca8ec91d46891062ff6fa3
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-23 13:19:01 -0500

    carrito modificado (problemas con el token)

--- frontend/src/components/Cart/Payment.jsx
+++ frontend/src/components/Cart/Payment.jsx
@@ -6,42 +6,9 @@ function Payment() {
   const dispatch = useDispatch()
   const cartItems = useSelector((state) => state.cart.items)
 
-  const onCheckoutPurchase = async () => {
-    let totalCompra = 0;
-
-    cartItems.forEach(producto => {
-      totalCompra += producto.Precio * producto.cantidad;
-    });
-    for (let item of cartItems) {
-      
-      const purchaseData = {
-        Titulo: item.Nombre,
-        UserId: item.userId,
-        ProductId: item.Id,
-        FechaCompra: new Date().toISOString(),
-        Cantidad: item.cantidad,
-        PrecioTotal: totalCompra
-      }
-      await dispatch(addPurchase(purchaseData))
-    }
-    // dispatch(clearCart())
-  }
-
+  
   return (
-    <section>
-      <h2 className='text-tlv text-general mt-[82px] mb-[31px]'>Seleccionar método de pago</h2>
-      <div className='flex flex-col gap-6'>
-        {/* <button className='h-16 w-[504px]  border-primary border-2 rounded-[10px] font-bold text-xl text-specific'>
-          Pagar con tarjeta
-        </button>
-        <button className='h-16 w-[504px]  border-primary border-2 rounded-[10px] font-bold text-xl text-specific'>
-          Pagar con Mercado Pago
-        </button> */}
-        <button className='h-16 w-[504px] bg-primary rounded-[10px] font-bold text-xl text-white' type='submit' onClick={onCheckoutPurchase}>
-          Comprar
-        </button>
-      </div>
-    </section>
+    <></>
   )
 }
 
