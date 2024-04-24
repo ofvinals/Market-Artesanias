96a8566451b548f6f3970efd8b2a86e7da8789b7
Oscar Frias Viñals <140022816+ofvinals@users.noreply.github.com>
2024-04-23 21:12:08 -0300

    corrijo newproduct

---
+++ frontend/src/components/SearchCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function SearchCard({ items }) {
+	console.log(items);
+	if (!items || items.length === 0) {
+		return (
+			<div className='flex items-center justify-center w-full mt-20'>
+				<img
+					src='/blue magnifying glass with resume sheet.svg'
+					alt='Imagen Buscar'
+				/>{' '}
+			</div>
+		);
+	}
+	console.log(items);
+
+	return (
+		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+			{items.map((item) => (
+				<article
+					key={item.Id}
+					className='flex flex-col border w-[296px] h-[261px] rounded-[10px] shadow-[0_4px_3px_0_rgba(0,0,0,0.25)] hover:scale-110 hover:transition-all'>
+					<Link to={`/${item.Id}`}>
+						<div>
+							<img
+								src={item.Imagen}
+								alt={item.Nombre}
+								className='w-[296px] h-[157px] object-cover rounded-t-[10px]'
+							/>
+						</div>
+						<div className='pl-5 pt-3 pb-3 bg-[#D9D9D94A]'>
+							<h2 className='text-xl font-bold text-general '>
+								{item.Nombre}
+							</h2>
+							<p className='text-general'>{item.Description}</p>
+							<p className='text-specific text-xl font-bold'>
+								$ {item.Precio}
+							</p>
+						</div>
+					</Link>
+				</article>
+			))}
+		</div>
+	);
+}
+
+export default SearchCard;
