1e0902433bf7525792d128e594f78afabb128488
Oscar Frias Viñals <140022816+ofvinals@users.noreply.github.com>
2024-04-25 18:45:05 -0300

    corrijo conexion back

--- backend/index.js
+++ backend/index.js
@@ -2,30 +2,30 @@ const server = require("./src/app");
 const { sequelize, Category } = require("./src/db");
 const PORT = process.env.PORT;
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
       server.listen(PORT, async () => {
-            await Category.bulkCreate([
-                  {
-                        Id: 1,
-                        Nombre: 'Vestimenta'
-                  },
-                  {
-                        Id: 2,
-                        Nombre: 'Cerámica'
-                  },
-                  {
-                        Id: 3,
-                        Nombre: 'Muebles'
-                  },
-                  {
-                        Id: 4,
-                        Nombre: 'Cuadros'
-                  },
-                  {
-                        Id: 5,
-                        Nombre: 'Accesorios'
-                  },
-            ])
+            // await Category.bulkCreate([
+            //       {
+            //             Id: 1,
+            //             Nombre: 'Vestimenta'
+            //       },
+            //       {
+            //             Id: 2,
+            //             Nombre: 'Cerámica'
+            //       },
+            //       {
+            //             Id: 3,
+            //             Nombre: 'Muebles'
+            //       },
+            //       {
+            //             Id: 4,
+            //             Nombre: 'Cuadros'
+            //       },
+            //       {
+            //             Id: 5,
+            //             Nombre: 'Accesorios'
+            //       },
+            // ])
             console.log(`Listen at ${PORT}`);
       });
 });
