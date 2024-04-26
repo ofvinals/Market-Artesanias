d62b391edcf260409d2f0845f7d436eaf829766d
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-25 23:12:59 -0500

    categoria en home responsive

--- frontend/src/components/Categorias.jsx
+++ frontend/src/components/Categorias.jsx
@@ -34,18 +34,18 @@ export default function Categorias() {
   }
 
   return (
-    <section className='w-full py-12 flex flex-col'>
+    <section className='w-full py-12 flex flex-col px-4 md:px-[104px] '>
       <h1 className='text-tlv font-light text-general'>Categorías</h1>
-      <div className='flex flex-wrap md:flex-row justify-evenly md:justify-between 2xl:justify-evenly mt-4'>
+      <div className='flex flex-wrap md:flex-row justify-around md:justify-between 2xl:justify-evenly mt-4'>
         {
           CATEGORIAS.map((categoria) => (
             <Link key={categoria.nombre} to='/categories' onClick={handleCategory(categoria.nombre)}>
               <div className='flex flex-col items-center relative group shadow-md rounded-[10px] mt-4 md:mt-0' >
                 <img src={categoria.imagen} alt={categoria.nombre} className='object-cover w-20 h-20 md:h-[356px] md:w-[228px] rounded-[10px]' />
-                  <h2 className='absolute pt-10 text-specific text-2xl hover:opacity-100 '>{categoria.nombre}</h2>
+                  <h2 className='absolute pt-10 text-specific md:text-2xl  text-sm hover:opacity-100 '>{categoria.nombre}</h2>
 
                 <div className='absolute inset-0 z-10 bg-black/40 hover:opacity-0 transition-opacity flex items-start justify-center rounded-[10px] pt-10'>
-                  <h2 className='text-white text-2xl  '>{categoria.nombre}</h2>
+                  <h2 className='text-white md:text-2xl  text-sm'>{categoria.nombre}</h2>
                 </div>
               </div>
             </Link>
