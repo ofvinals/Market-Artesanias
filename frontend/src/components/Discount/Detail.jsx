b8bf2532abd4a7bdefd990a9bc63deafc8a03aa6
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-25 23:33:54 -0500

    detalle de descuento responsive

--- frontend/src/components/Discount/Detail.jsx
+++ frontend/src/components/Discount/Detail.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { FaShoppingCart } from "react-icons/fa";
 import { useDispatch, useSelector } from 'react-redux';
-import { getProductById } from '../../redux/Slices/productSlice';
+import { deleteDetail, getProductById } from '../../redux/Slices/productSlice';
 import { addItem } from '../../redux/Slices/cartSlice';
-
+import Swal from 'sweetalert2';
 
 
 function Detail() {
@@ -16,6 +16,7 @@ function Detail() {
 
     useEffect(() => {
         dispatch(getProductById(id))
+        return (() => dispatch(deleteDetail()))
     }, [id, dispatch])
 
     const handleAddandNavigateToCart = () => {
@@ -29,7 +30,12 @@ function Detail() {
     const handleAddToCart = () => {
         if (productDetail) {
             dispatch(addItem(productDetail))
-
+            Swal.fire({
+                title: 'Producto agregado al carrito',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 2000,
+            })
         }
     }
 
@@ -39,28 +45,20 @@ function Detail() {
     }
 
     return (
-        <section className='grid grid-cols-2 mt-[74px] gap-[91px] 2xl:gap-0 2xl:mb-10'>
-            <div key={productDetail.Id} className='flex flex-row w-[610px] h-[461px] gap-3'>
-                <img src={productDetail.Imagen} alt={productDetail.Nombre} className=' rounded-[10px] object-cover h-[461px] w-[622px] shadow-[0_4px_3px_0_rgba(0,0,0,0.25)]' />
-            </div>
+        <section className='flex flex-col md:grid md:grid-cols-2 md:mt-[74px] md:gap-[91px] 2xl:gap-0 mb-10'>
+            <img src={productDetail.Imagen} alt={productDetail.Nombre} className=' rounded-[10px] object-cover h-[308px] md:h-[461px] md:w-[622px] shadow-[0_4px_3px_0_rgba(0,0,0,0.25)] mt-4 md:mt-0' />
             <div className=' '>
-                <h2 className='text-3tl text-general mb-4 2xl:mb-8'>{productDetail.Nombre}</h2>
-
-                <div className='flex flex-row items-center gap-7 mb-6 2xl:mb-8'>
-                    {/* <img src={productDetail.Store.Imagen} alt={productDetail.Store.Nombre} className=' size-[71px] rounded-full' /> */}
-                    {/* <p className='text-xl font-bold text-specific'>{productDetail.Store.Nombre}</p> */}
-                </div>
-
-                <p className='text-general text-xl mb-2 2xl:mb-4'>{productDetail.Descripcion}</p>
-                <p className='text-general text-2xl font-bold'>$ {productDetail.Precio - (productDetail.Precio*0.2)}</p>
+                <h2 className='text-xl md:text-3tl mt-3 md:mt-0 font-semibold md:font-normal text-general md:mb-4 2xl:mb-8'>{productDetail.Nombre}</h2>
+                <p className='text-general mt-2 md:mt-0 md:text-xl md:mb-2 2xl:mb-4'>{productDetail.Descripcion}</p>
+                <p className='text-specific md:text-general mt-4 md:mt-0 text-2xl font-bold'>$ {productDetail.Precio - (productDetail.Precio * 0.2)}</p>
                 <div className='mt-6 flex flex-row gap-8'>
                     <div className='flex flex-row gap-[65px]'>
                         <p className='text-general text-2xl font-bold'>Disponible: {productDetail.Disponible}</p>
                     </div>
                 </div>
-                <div className='flex flex-row gap-[14px] mt-[31px]'>
-                    <button className='h-[44px] w-[260px] rounded-[5px] bg-primary text-white' onClick={handleAddandNavigateToCart}>Comprar </button>
-                    <button className='h-[44px] w-[260px] rounded-[5px] border-2 border-primary text-specific items-center flex justify-center gap-[10px]' onClick={handleAddToCart}>Agregar al carrito <FaShoppingCart /></button>
+                <div className='flex flex-col md:flex-row gap-[14px] mt-[31px]'>
+                    <button className='h-[44px] md:w-[260px] rounded-[5px] bg-specific text-white' onClick={handleAddandNavigateToCart}>Comprar </button>
+                    <button className='h-[44px] md:w-[260px] rounded-[5px] border-2 border-specific text-specific items-center flex justify-center gap-[10px]' onClick={handleAddToCart}>Agregar al carrito <FaShoppingCart /></button>
                 </div>
             </div>
         </section>
