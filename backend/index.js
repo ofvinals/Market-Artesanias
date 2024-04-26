cc15389e031da30c18d08b36289156f04d3d6dd6
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-26 09:25:39 -0500

    condición en la categoría cuando se inicia el proyecto

--- backend/index.js
+++ backend/index.js
@@ -2,30 +2,38 @@ const server = require("./src/app");
 const { sequelize, Category } = require("./src/db");
 const PORT = process.env.PORT;
 
+
+
 sequelize.sync({ force: false }).then(() => {
       server.listen(PORT, async () => {
-            // await Category.bulkCreate([
-            //       {
-            //             Id: 1,
-            //             Nombre: 'Vestimenta'
-            //       },
-            //       {
-            //             Id: 2,
-            //             Nombre: 'Cerámica'
-            //       },
-            //       {
-            //             Id: 3,
-            //             Nombre: 'Muebles'
-            //       },
-            //       {
-            //             Id: 4,
-            //             Nombre: 'Cuadros'
-            //       },
-            //       {
-            //             Id: 5,
-            //             Nombre: 'Accesorios'
-            //       },
-            // ])
+            const allCategories = await Category.findAll()
+            if (allCategories.length < 1) {
+                  await Category.bulkCreate([
+                        {
+                              Id: 1,
+                              Nombre: 'Vestimenta'
+                        },
+                        {
+                              Id: 2,
+                              Nombre: 'Cerámica'
+                        },
+                        {
+                              Id: 3,
+                              Nombre: 'Muebles'
+                        },
+                        {
+                              Id: 4,
+                              Nombre: 'Cuadros'
+                        },
+                        {
+                              Id: 5,
+                              Nombre: 'Accesorios'
+                        },
+                  ])
+                  console.log('Categorías guardadas');
+            } else {
+                  console.log('Ya existen las categorías');
+            }
             console.log(`Listen at ${PORT}`);
       });
 });
