8397e059530b1d465f79c9c25edddad1d79fc76f
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-25 22:51:53 -0500

    modifico la condición para agregar las categorías al iniciar el servidor

--- backend/index.js
+++ backend/index.js
@@ -6,28 +6,34 @@ const PORT = process.env.PORT;
 
 sequelize.sync({ force: false }).then(() => {
       server.listen(PORT, async () => {
-            await Category.bulkCreate([
-                  {
-                        Id: 1,
-                        Nombre: 'Vestimenta'
-                  },
-                  {
-                        Id: 2,
-                        Nombre: 'Cerámica'
-                  },
-                  {
-                        Id: 3,
-                        Nombre: 'Muebles'
-                  },
-                  {
-                        Id: 4,
-                        Nombre: 'Cuadros'
-                  },
-                  {
-                        Id: 5,
-                        Nombre: 'Accesorios'
-                  },
-            ])
+            const allCategories = await Category.findAll()
+            if (allCategories.length < 1) {
+                  await Category.bulkCreate([
+                        {
+                              Id: 1,
+                              Nombre: 'Vestimenta'
+                        },
+                        {
+                              Id: 2,
+                              Nombre: 'Cerámica'
+                        },
+                        {
+                              Id: 3,
+                              Nombre: 'Muebles'
+                        },
+                        {
+                              Id: 4,
+                              Nombre: 'Cuadros'
+                        },
+                        {
+                              Id: 5,
+                              Nombre: 'Accesorios'
+                        },
+                  ])
+                  console.log('Categorías guardadas');
+            } else {
+                  console.log('Ya existen las categorías');
+            }
             console.log(`Listen at ${PORT}`);
       });
 });
