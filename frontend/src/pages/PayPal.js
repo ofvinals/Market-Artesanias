dedc5ae1589cf8ea778cfac8cd3689ef8c2bdc91
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-04-25 22:49:10 -0500

    condiciones agregadas

--- frontend/src/pages/PayPal.js
+++ frontend/src/pages/PayPal.js
@@ -1,47 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 
-function PayPal() {
+function Tarjeta() {
     const navigate = useNavigate()
+    const [numeroTarjeta, setNumeroTarjeta] = useState('');
+    const [fechaExpiracion, setFechaExpiracion] = useState('');
+    const [cvv, setCvv] = useState('');
 
-    const handleSubmit = () => {
-        Swal.fire({
-            title: "Excelente!",
-            text: "Compra realizada",
-            icon: "success",
-            showConfirmButton: false,
-            timer: 2000,
-        })
-        navigate('/');
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        if (numeroTarjeta.trim() === '' || fechaExpiracion.trim() === '' || cvv.trim() === '') {
+
+            Swal.fire({
+                title: 'Error',
+                text: 'Por favor completa todos los campos',
+                icon: 'error',
+            })
+        } else {
+            Swal.fire({
+                title: 'Excelente!',
+                text: 'Compra realizada',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 2000,
+            })
+            navigate('/');
+        }
+
     }
 
     return (
         <>
             <NavBar />
-            <div className='flex flex-row mx-[104px] md:gap-[109px] 2xl:justify-evenly mt-5 items-center'>
+            <div className='flex flex-col md:flex-row px-5  md:mx-[104px] md:gap-[109px] 2xl:justify-evenly mt-5 items-center'>
                 <div className='flex flex-col'>
-                    <h1 className='text-tlv text-general '>Ingresar datos de Paypal</h1>
-                    <form className='flex flex-col mt-9' id='tarjeta' onSubmit={handleSubmit}>
-                        <label className='text-xl text-general mb-3'>
+                    <h1 className='text-xl  md:text-tlv text-general '>Ingresar datos de Paypal</h1>
+                    <form className='flex flex-col mt-4 md:mt-9' id='tarjeta' onSubmit={handleSubmit}>
+                        <label className=' text-sm md:text-xl text-general mb-2 md:mb-3'>
                             Número de tarjeta
                         </label>
-                        <input className='w-[504px] h-16  text-xl border-2 border-specific mb-7 rounded-xl p-2 ' type='text' />
-                        <label className='text-xl text-general mb-3'>
+                        <input className='w-[366px] md:w-[504px] h-12 md:h-16 text-sm md:text-xl border-2 border-specific mb-7 rounded-md md:rounded-[10px] p-2 ' type='text' value={numeroTarjeta} onChange={(event) => setNumeroTarjeta(event.target.value)} />
+                        <label className=' text-sm md:text-xl text-general mb-2 md:mb-3'>
                             Fecha de expiración
                         </label>
-                        <input className='w-[504px] h-16  text-xl border-2 border-specific mb-7 rounded-xl p-2 ' type='text' />
-                        <label className='text-xl text-general mb-3'>
+                        <input className='w-[366px] md:w-[504px] h-12 md:h-16 text-sm md:text-xl border-2 border-specific mb-7 rounded-md md:rounded-[10px] p-2 ' type='text' value={fechaExpiracion} onChange={(event) => setFechaExpiracion(event.target.value)} />
+                        <label className=' text-sm md:text-xl text-general mb-2 md:mb-3'>
                             CVV No.
                         </label>
-                        <input className='w-[504px] h-16  text-xl border-2 border-specific mb-7 rounded-xl p-2 ' type='text' />
-                        <button className='h-16 w-[504px] bg-primary rounded-[10px] hover:bg-[#0739EB] font-bold text-xl text-white' type='submit' >
+                        <input className='w-[366px] md:w-[504px] h-12 md:h-16 text-sm md:text-xl border-2 border-specific mb-7 rounded-md md:rounded-[10px] p-2 ' type='text' value={cvv} onChange={(event) => setCvv(event.target.value)} />
+                        <button className='h-12 md:h-16 w-[366px] md:w-[504px] bg-specific rounded-md md:rounded-[10px] hover:bg-[#0739EB] font-bold  text-sm md:text-xl text-white' type='submit' >
                             Comprar
                         </button>
                     </form>
                 </div>
-                <div className='mt-[150px]'>
+                <div className='mt-[150px] hidden md:flex'>
                     <img src='./payment.svg' alt='payment' />
                 </div>
             </div>
@@ -49,4 +64,4 @@ function PayPal() {
     )
 }
 
-export default PayPal
+export default Tarjeta
